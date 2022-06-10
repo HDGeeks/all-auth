79f2dc220ce3dbd9d2b086a183ccb4d1f0877bb5
Daniel Tesfai <dannyhd88@gmail.com>
2022-06-10 14:10:50 +0300

    beta

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.environ.get('SECRET_KEY')
+# SECRET_KEY=os.environ.get('SECRET_KEY')
+SECRET_KEY="django-insecure-as*@^rs!=q4id1@9chogtm!_&l1bp6#mjsnf_1+cpamh10mirn"
+
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
@@ -140,8 +142,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 DEFAULT_TO_EMAIL = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_USER =os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER =os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_HOST_USER ='kinideas.tech@gmail.com'
+EMAIL_HOST_PASSWORD ='kcufxkucwndjreua'
 
 
 
